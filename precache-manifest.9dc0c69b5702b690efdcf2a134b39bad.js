29f013f24b00964f15d38288bd19c4708cbe5ab1
SDubaj <dubajsebastian96@gmail.com>
2020-05-06 22:35:34 +0200

    Updates

---
+++ precache-manifest.9dc0c69b5702b690efdcf2a134b39bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bd6a2a0a41d22678b7593d56613430",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "56c20d691671853e1c22",
+    "url": "/MemoryGame/static/css/main.e565e28b.chunk.css"
+  },
+  {
+    "revision": "42e4826a8909dd1ec2fa",
+    "url": "/MemoryGame/static/js/2.631e315c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemoryGame/static/js/2.631e315c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c20d691671853e1c22",
+    "url": "/MemoryGame/static/js/main.0c0e3a03.chunk.js"
+  },
+  {
+    "revision": "bdba029f7b7d9b17546d",
+    "url": "/MemoryGame/static/js/runtime-main.14f3a3ae.js"
+  }
+]);
